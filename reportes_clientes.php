9b442f76a37f9175e324a5d20b2e73d747ebe39a
Jorge Brunal <diniremix@gmail.com>
2015-06-01 16:52:51 -0500

    cambio a minusculas, para evitar conflictos en sistemas tipo unix

--- reportes_clientes.php
+++ reportes_clientes.php
@@ -1,4 +1,4 @@
-<?php require_once('Connections/conexion.php'); ?>
+<?php require_once('connections/conexion.php'); ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
