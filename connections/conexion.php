fee02acf801661f6d5fe900ccdd27a3a5200f23d
Jorge Brunal <diniremix@gmail.com>
2015-06-01 17:04:49 -0500

    actualizacion de datos para la conexion

--- connections/conexion.php
+++ connections/conexion.php
@@ -2,9 +2,20 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
+
+//localhost
 $hostname_conexion = "localhost";
-$database_conexion = "parqueadero";
+$database_conexion = "parkingdb";
 $username_conexion = "root";
 $password_conexion = "";
+
+//server
+/*
+$hostname_conexion = "";
+$database_conexion = "";
+$username_conexion = "";
+$password_conexion = "";
+*/
+
 $conexion = mysql_pconnect($hostname_conexion, $username_conexion, $password_conexion) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
